d24006e00d6ba138b566ec43634ca7fe3579fba8
Candace <goodsoncj@gmail.com>
2021-01-13 21:28:52 -0500

    in class mileage form

---
+++ MVCLab/Controllers/GasStation.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCLab.Models.GasStation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCLab.Controllers
+{
+    public class GasStation : Controller
+    {
+        public IActionResult MileageForm()
+        {
+
+            var model = new MileageFormResultsViewModel(); 
+
+            return View(model);
+        }
+
+        public IActionResult MileageFormResults(MileageFormResultsViewModel model)
+        {
+
+            model.remainingMiles = model.Gallons * model.Mileage;
+
+            model.enoughGas = model.remainingMiles > model.TripDistance;
+
+            if (model.enoughGas)
+            {
+                model.userNotification = "You have enough gas for your trip!!!!";
+            }
+            else
+            {
+                model.userNotification = "You do NOT have enough gas for your trip :(";
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ MVCLab/Models/GasStation/MileageFormResultsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCLab.Models.GasStation
+{
+    public class MileageFormResultsViewModel
+    {
+        public int Mileage { get; set; }
+        public int Gallons { get; set; }
+
+        public int remainingMiles { get; set; }
+
+        public int TripDistance { get; set; }
+
+        public bool enoughGas { get; set; }
+
+        public string userNotification { get; set; }
+    }
+}
